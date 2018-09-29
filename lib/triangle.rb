c5b0d35bd7bcc37a77487995e094af98d05f0cf4
Ashley Bland <ashleyjbland@gmail.com>
2018-09-29 15:24:15 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,26 @@
 class Triangle
-  # write code here
+  
+
+  def initialize(a, b, c)
+    @a = a
+    @b = b
+    @c = c
+  end
+
+  def kind
+    if side1 + side2 || side1 + side3 || side2 + side3 > side1 || side2 || side3
+      begin
+        raise TriangleError
+      rescue TriangleError => error
+        puts error.message
+      end
+    end
+  end
+
+  class TriangleError < StandardError
+    def message
+      "The sum of the lengths of any two sides of a triangle always exceeds the length of the third side."
+    end
+  end
+  
 end
